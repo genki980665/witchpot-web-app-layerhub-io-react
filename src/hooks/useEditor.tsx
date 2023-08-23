f52e3bc1f5f1e40cc7add2d315da0e923f660047
Genki Nakabayashi <nakabayashi.genki@witchpot.com>
2023-08-23 20:51:49 +0900

    fixed compile error

--- src/hooks/useEditor.tsx
+++ src/hooks/useEditor.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Context } from "../context";
+import { Editor } from "@layerhub-io/core";
 
-export function useEditor() {
+export function useEditor() : Editor {
   const { editor } = React.useContext(Context);
   return editor;
 }
